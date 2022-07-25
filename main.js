a7971b2411487a5d3535be4c83b25034669e3fff
KonstantinEw <Ewwel@inbox.ru>
2022-07-25 13:15:19 +0300

    2,3,4

--- main.js
+++ main.js
@@ -19,18 +19,25 @@ console.log(typeof varNum);
 let height = 15;
 let width = 20;
 
-if(width>height) {
-    console.log (width);
+if(height>width) {
+    console.log (height);
+} else if (height===width) {
+    console.log('Числа равны');
+} else(height<width); {
+    console.log(width);
 };
 
 //3
 
-for(let i = 1; i++;) {
-    if (i % 3 ===0 )
+
+
+for(let i = 1; i<=20; i++) {
+    if (i % 3 == 0 ) {
         console.log(i);
-    else if (i >= 20)
-    break;
+    }
 };
+
+
 
 //4
 
@@ -40,7 +47,7 @@ let pen = true
 let apple = false
 let orange = true
 
-let shouldGoToWork = (key && documents && pen && (apple || orange));
+let shouldGoToWork = key && documents && pen && (apple || orange);
 
 console.log (shouldGoToWork);
 
@@ -60,56 +67,56 @@ console.log (shouldGoToWork);
 
 //6
 
-let age = prompt('Введите ваш возраст:');
-if (age > 18) {
-    alert('Попей пивка');
-} else if ((age <= 18) && (age == 16 || age == 17 || age == 18)) {
-    alert('Пей колу');
-    alert('Можешь выкурить сигаретку, только маме не говори')
-} else (alert('Пей колу'));
+// let age = prompt('Введите ваш возраст:');
+// if (age > 18) {
+//     alert('Попей пивка');
+// } else if ((age <= 18) && (age == 16 || age == 17 || age == 18)) {
+//     alert('Пей колу');
+//     alert('Можешь выкурить сигаретку, только маме не говори')
+// } else (alert('Пей колу'));
 
 //7
 
-let side = prompt('Куда вы хотели бы отправится?')
+// let side = prompt('Куда вы хотели бы отправится?')
 
-switch (side) {
-    case 'юг':
-        alert('на юг пойдешь счастье найдешь')
-        break;
-    case 'север':
-        alert('на север пойдешь много денег найдешь')
-        break
-    case 'запад':
-        alert('на запад пойдешь верного друга найдешь')
-        break
-    case 'восток':
-        alert('на восток пойдешь разработчиком станешь')
-        break
-    default:
-        alert('Тур не предусмотрен')
-};
+// switch (side) {
+//     case 'юг':
+//         alert('на юг пойдешь счастье найдешь')
+//         break;
+//     case 'север':
+//         alert('на север пойдешь много денег найдешь')
+//         break
+//     case 'запад':
+//         alert('на запад пойдешь верного друга найдешь')
+//         break
+//     case 'восток':
+//         alert('на восток пойдешь разработчиком станешь')
+//         break
+//     default:
+//         alert('Тур не предусмотрен')
+// };
 
-8
-let userName = 'пОлИнА нАбЕрЕжНаЯ'
-let userNameSecond = prompt('Enter your name:', userName);
-if (userName == 'пОлИнА нАбЕрЕжНаЯ') {
-    alert('Привет, Полина Набережная!');
-} else (alert('Нет такого пользователя'));
+// 8
+// let userName = 'пОлИнА нАбЕрЕжНаЯ'
+// let userNameSecond = prompt('Enter your name:', userName);
+// if (userName == 'пОлИнА нАбЕрЕжНаЯ') {
+//     alert('Привет, Полина Набережная!');
+// } else (alert('Нет такого пользователя'));
 
 // let userNameAlert = userName[0].toUpperCase()+userName.slice(0);
 // alert('Привет,'+' '+userNameAlert+'!');
 
 //9
 
-let userNumber = prompt('Введите число:', '');
-let userMinus = prompt ('Сколько отнять?', '');
-let userPlus = prompt ('Сколько прибавить?', '');
-let userMultiplication = prompt ('На сколько умножить?', '');
-let userDivision = prompt ('На сколько разделить?', '');
+// let userNumber = prompt('Введите число:', '');
+// let userMinus = prompt ('Сколько отнять?', '');
+// let userPlus = prompt ('Сколько прибавить?', '');
+// let userMultiplication = prompt ('На сколько умножить?', '');
+// let userDivision = prompt ('На сколько разделить?', '');
 
-let result = ((userNumber-userMinus+userPlus)*userMultiplication/userDivision);
+// let result = ((userNumber-userMinus+userPlus)*userMultiplication/userDivision);
 
-alert(`((((${userNumber} - ${userMinus}) + ${userPlus}) * ${userMultiplication}) / ${userDivision} = ${result})`);
+// alert(`((((${userNumber} - ${userMinus}) + ${userPlus}) * ${userMultiplication}) / ${userDivision} = ${result})`);
 
 //10
 
